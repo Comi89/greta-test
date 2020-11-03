87e66ec96c411260bfeb87a0ba83fbbdc3a4cc56
Mihajlo Grubjesic (IC) <mihajlo_grubjesic@apple.com>
2020-11-03 21:27:12 +0100

    display city pm10 value

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
+function renderPM10Value(data, city) {
+  dataSection.innerHTML =
+    "<span class='air_pollution-city'> pm10 value: </span><span class='air_pollution-value'></span>";
+  var citySection = document.querySelector(".air_pollution-city");
+  var valueSection = document.querySelector(".air_pollution-value");
+  citySection.prepend(city);
+  valueSection.append(data.value + " " + data.unit);
+}
 
+function fetchPollutionData(city) {
+  dataSection.append("Loading...");
+  fetch("https://api.openaq.org/v1/measurements?parameter=pm10&city=" + city)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (value) {
+      renderPM10Value(value.results[0], city);
+    })
+    .catch(function (err) {
+      console.warn("Something went wrong.", err);
+    });
+}
+
+var dataSection = document.querySelector(".air_pollution-section");
+
+fetchPollutionData("Porto");
